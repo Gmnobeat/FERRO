98b160f7f851d982ee682ce0deef6af5fca59cc1
Gustavo Malacrida Mazetti <gustavow0703@gmail.com>
2026-02-26 17:56:28 -0300

    Comecei o Exercício passado em aula

---
+++ Terceiro Termo/Primeiro Bimestre/Exercicio4Lista1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        int milisegundo = 73230000,dia,horas,minutos,segundos;
+
+        System.out.println(milisegundo);
+    }
+}
